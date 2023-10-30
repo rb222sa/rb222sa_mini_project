2ed07a3149c30f79b2fa8ee5e194d659424bc366
Ricard Bengtsson <rb222sa@student.lnu.se>
2023-10-30 18:43:09 +0100

    Lawmower func issue fixed
    delta T was wrong

--- lawnmower.py
+++ lawnmower.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 import time
+import math
+from random import uniform
 
 
 small_lawn_map = [[1,1,0,1,1],
@@ -39,7 +41,7 @@ big_lawn_map =  [[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
 [1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
 [1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]]
 
-current_lawn_map = big_lawn_map
+current_lawn_map = small_lawn_map
 rows = len(current_lawn_map)
 cols = len(current_lawn_map[0])
 
@@ -52,36 +54,54 @@ ax.set_yticks(range(0, rows+1, 1))
 ax.set_xticks(range(0, cols+1, 1))
 plt.title(f"Colored grid of size {rows}x{cols}")
 
-
+angle = 2*math.pi
 pos_x = 0.0
 pos_y = 0.0
 v_x = 0.3
 v_y = 0.3
 start = time.time()
+previous_time = start
+print(rows, cols)
 
 
 #   Compute new position by using given equation in assignment pdf,
 #   "x^i+1 = x^i + vx∆t" 
 def update_pos(current_time, pos_x, pos_y, v_x, v_y):
-    new_x = pos_x + (v_x * (current_time - start))     
-    new_y = pos_y + (v_y * (current_time - start))
+    global previous_time
+    elapsed_time = current_time - previous_time
+    new_x = pos_x + (v_x * (elapsed_time))     
+    new_y = pos_y + (v_y * (elapsed_time))
+    previous_time = current_time
     return new_x, new_y
 
 
-def check_collision():
-    return
+def check_collision(pos_x, pos_y, v_x, v_y):
+    if int(pos_x) >= 5:
+        v_x = math.cos(uniform(0.0,-1.0))
+        v_y = math.sin(uniform(0.0, 1.0))
+    if int(pos_x) < 0:
+        v_x = math.cos(uniform(0.0, 1.0))
+        v_y = math.sin(uniform(0.0, 1.0))
+    if int(pos_y) > 4:
+        v_x = math.cos(uniform(0.0, 1.0))
+        v_y = math.sin(uniform(0.0, -1.0))
+    if int(pos_y) < 0:
+        v_x = math.cos(uniform(0.0, 1.0))
+        v_y = math.sin(uniform(0.0, 1.0))
+    return v_x, v_y
 
 
 # Starting the lawnmower
-for i in range(0, 1000):
+for i in range(0, 20):
     pos_x, pos_y = update_pos(time.time(), pos_x, pos_y, v_x, v_y)
+    v_x, v_y = check_collision(pos_x, pos_y, v_x, v_y)
     print(f"x: {pos_x}\t y:{pos_y}")
+    time.sleep(1)
 
 
-computingtime = time.time() - start
+computingtime = time.time() - previous_time
 print(computingtime)
 
-plt.show()
 
 
 # https://www.geeksforgeeks.org/python-time-module/
--- test3.py
+++ test3.py
@@ -1,30 +1,4 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.animation
+from random import uniform
 
 
-
-x_data = []
-y_data = []
-
-fig, ax = plt.subplots()
-ax.set_xlim(0, 205)
-ax.set_ylim(0, 12)
-line, = ax.plot(0, 0)
-
-
-def animation_frames(frame):
-    x_data.append(frame * 10)
-    y_data.append(frame)
-
-    line.set_xdata(x_data)
-    line.set_ydata(y_data)
-    return line,
-
-
-animation = matplotlib.animation.FuncAnimation(fig, func=animation_frames, frames=np.arange(0, 5, 0.1), interval=10)
-
-plt.show()
-
-
-# https://www.youtube.com/watch?v=dOKHY_PUvqU
+print(round(uniform(0.0, -1.0),2 ))
